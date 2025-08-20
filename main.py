b52daf04056287ef78b7c067fd2a1d34c375bd1d
Christian Richardt <christian.bjork.richardt@gmail.com>
2025-08-20 20:08:02 +0200

    merge

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import os
+
+generel_id = 1407704842004070443
+image_types = {"png", "jpeg", "jpg", "pdf"}
+
+class Client(discord.Client):
+    
+    async def on_ready(self):
+        os.system("echo Penis")
+        print(f'{self} online!')
+        channel = Client.get_channel(self, generel_id)
+        await channel.send('Online!')
+    
+    async def on_message(self, message):
+        for attachment in message.attachments:
+            if any(attachment.filename.lower().endswith(image) for image in image_types):
+                await attachment.save(f'attachments/{attachment.filename}')
+                os.system(f'lp {attachment.filename}')
+                channel = Client.get_channel(self, generel_id)
+                await channel.send('Starting to print!')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run('BOT_TOKEN_HERE')
