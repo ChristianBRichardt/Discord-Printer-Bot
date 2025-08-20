782e9e80a6e9124acf213918f6c6024409a0c9f3
Christian Richardt <christian.bjork.richardt@gmail.com>
2025-08-20 20:01:34 +0200

    plz fucking work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 generel_id = 1407704842004070443
 image_types = {"png", "jpeg", "jpg", "pdf"}
@@ -6,17 +7,18 @@ image_types = {"png", "jpeg", "jpg", "pdf"}
 class Client(discord.Client):
     
     async def on_ready(self):
-        channel = Client.get_channel(self, generel_id)
-        await channel.send('Online!')
+        os.system("echo Penis")
+        print(f'{self} online!')
+        # channel = Client.get_channel(self, generel_id)
+        # await channel.send('Online!')
     
     async def on_message(self, message):
         for attachment in message.attachments:
             if any(attachment.filename.lower().endswith(image) for image in image_types):
                 await attachment.save(f'attachments/{attachment.filename}')
+                os.system(f'lp {attachment.filename}')
                 channel = Client.get_channel(self, generel_id)
                 await channel.send('Starting to print!')
-
-                
 
 intents = discord.Intents.default()
 intents.message_content = True
